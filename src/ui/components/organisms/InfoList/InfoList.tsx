3d09b4e3c4755548bdc5e104734e3067d533f5b0
George <george.valuiskyi@gmail.com>
2025-01-06 19:29:40 +0200

    feat: info list
    Add InfoList component.

---
+++ src/ui/components/organisms/InfoList/InfoList.tsx
@@ -0,0 +1,37 @@
+import { TInfoItem } from "@/types/models/info-items.ts";
+import { InfoListItem } from "@/ui/components/molecules/InfoListItem";
+import { List } from "@mui/material";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
+import { FC } from "react";
+
+type Props = {
+    title: string;
+    items: TInfoItem[];
+};
+
+export const InfoList: FC<Props> = ({ title, items }) => {
+    return (
+        <Box>
+            <Typography id="modal-modal-title" variant="h5" component="h2">
+                {title}
+            </Typography>
+            <List
+                sx={{
+                    listStyleType: "decimal",
+                    pl: 2,
+                    maxHeight: "400px",
+                    overflowY: "auto",
+                }}
+            >
+                {items.map(({ title, description }) => (
+                    <InfoListItem
+                        key={title}
+                        title={title}
+                        description={description}
+                    />
+                ))}
+            </List>
+        </Box>
+    );
+};
