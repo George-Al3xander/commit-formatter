13c5eb620fef0aad401da5f05742330c22ab22b2
George <george.valuiskyi@gmail.com>
2025-01-07 16:48:56 +0200

    feat: clipboard
    Add copyToClipboard util function and implement tests.

---
+++ src/utils/helpers/clipboard.ts
@@ -0,0 +1,12 @@
+import { COPY_MESSAGES } from "@/utils/consts.ts";
+import toast from "react-hot-toast";
+
+export const copyToClipboard = async (text: string) => {
+    try {
+        await navigator.clipboard.writeText(text);
+        toast.success(COPY_MESSAGES.success);
+    } catch (e) {
+        console.error(e);
+        toast.error(COPY_MESSAGES.error);
+    }
+};
